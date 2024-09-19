25e08f0cc75d2a5bbd35ab8ec9c505a2453b188c
Ymnich <vx454992@gmail.com>
2024-09-19 09:55:21 +0300

    add style and js 3

--- videolka/views.py
+++ videolka/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from video import models
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/index.html')
+    videos = models.Video.objects.all()
+    context = {'videos': videos}
+    return render(request, 'main/index.html', context)
