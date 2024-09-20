c85c1f8ccc6e84e4c79bfad22dab67b9ee19388c
stud <vx454992@gmail.com>
2024-09-20 09:44:56 +0300

    Addd 'login' app

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'videolka', 'video',
+    'videolka', 'video', "login",
 ]
 
 MIDDLEWARE = [
