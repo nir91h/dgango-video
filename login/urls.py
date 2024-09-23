84567e7b328a7c63379e870b0f2c88e54cef3d34
stud <vx454992@gmail.com>
2024-09-23 13:29:02 +0300

    aut add

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path('login/', views.login_view),
-    path('register/', views.register)
+    path('register/', views.register),
+    path('logout/', views.logout_view)
 ]
